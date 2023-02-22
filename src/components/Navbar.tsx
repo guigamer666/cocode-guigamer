e7a6b9dc42f856e75fb020874cb2d8603cc5df04
jlochoa <jlprofesor8@gmail.com>
2023-02-22 18:34:01 +0100

    Navbar colapsable

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,22 @@ import { Link, NavLink } from 'react-router-dom';
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
       <Link className="navbar-brand" to="/">
         Código JL
       </Link>
 
-      <div className="collapse navbar-collapse">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav">
           <li className="nav-item">
             <NavLink className={({ isActive }) => `nav-item nav-link  ${isActive ? 'active' : ''}`} to="/">
